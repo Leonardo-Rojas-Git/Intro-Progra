630ab85bd7f2c0088d9b086c7dca5a0d1e44bfd4
Leonardo Rojas <leor43993@gmail.com>
2026-08-28 08:36:33 -0500

    Add files via upload
    Semana 1: operadores matemáticos y funciones

---
+++ S1 funciones y operadores.py
@@ -0,0 +1,9 @@
+
+radio = 34.56
+
+area = 3.141*radio**2
+#area = round(area,4)
+print(f"el area de circulo es {area:.3f}") #el formato de mi  variable {variable.3f}
+#division entrera: //
+#residuo: %
+#import math
