225ae31550734153094c6edfc15ab3ba60a99eba
Leonardo Rojas <leor43993@gmail.com>
2026-09-02 08:44:52 -0500

    Update S1 funciones y operadores.py

--- S1 funciones y operadores.py
+++ S1 funciones y operadores.py
@@ -1,9 +1,18 @@
-
 radio = 34.56
 
 area = 3.141*radio**2
 #area = round(area,4)
-print(f"el area de circulo es {area:.3f}") #el formato de mi  variable {variable:.3f} #dinero es < :.2f >
+
+print(f"el area de circulo es {area:.3f}")
 #division entrera: //
 #residuo: %
 #import math
+
+TIPS EE:
+#el formato de mi  variable {variable:.3f} #dinero es < :.2f >
+
+
+S2 #ALgoritmos, Pasar de pseudocodigo a Programa
+TIPS EE:
+#Usa el nombre de la variable que te dicen en el pseudocodigo <EE>
+
